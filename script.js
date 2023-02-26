2fbba3acdcc558f38b290644e892eb3b1dc08086
itz-vikash-gupta <viku9211gupta@gmail.com>
2023-02-26 12:06:59 +0530

    d

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+let button=document.getElementById('swap');
+let body=document.getElementsByTagName('body')
+let main=document.getElementById("app")
 
+function swap(){
+    if(button.getAttribute("class")==="button_day"){
+    document.body.style.backgroundColor = "dark";
+    button.setAttribute("class", "button_night")
+    main.setAttribute("class","night")
+    }
+    else{
+        document.body.style.backgroundColor = "light";
+        button.setAttribute("class", "button_day")
+        main.setAttribute("class","day") 
+    }
+
+}
